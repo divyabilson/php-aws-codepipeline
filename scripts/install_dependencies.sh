e0c758588783387825f7a82a3ac4d78cb87b622e
divyabilson <137600081+divyabilson@users.noreply.github.com>
2024-02-17 18:04:33 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if ! [ -x "$(command -v apache2)" ]; then apt-get update && apt-get install -y apache2 >&2; exit 1; fi
+#if ! [ -x "$(command -v apache2)" ]; then apt-get update && apt-get install -y apache2 >&2; exit 1; fi
 
 
